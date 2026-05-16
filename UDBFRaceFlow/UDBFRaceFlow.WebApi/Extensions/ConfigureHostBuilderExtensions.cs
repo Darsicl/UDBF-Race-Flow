84ee38e145033a9c2692f745d9d9f74b0d6c2fae
Darsicl <scorpion20022007@gmail.com>
2026-05-16 12:14:39 +0200

    feat: Create extension for host services, add configuration for serilog, register serilog in program

---
+++ UDBFRaceFlow/UDBFRaceFlow.WebApi/Extensions/ConfigureHostBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Serilog;
+
+namespace UDBFRaceFlow.WebApi.Extensions
+{
+    public static class ConfigureHostBuilderExtensions
+    {
+        public static void ConfigureSerilog(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            builder.Host.UseSerilog((ctx, services, loggerCofiguration) =>
+            {
+                loggerCofiguration.ReadFrom.Configuration(builder.Configuration);
+            });
+        }
+    }
+}
